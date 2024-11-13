50b7c37ef7dce277e716eccb3fb4a3762d58fa4b
lcwat <lcwatson012@gmail.com>
2024-11-12 19:46:54 -0600

    initialize the repo

---
+++ scripts/exercise-11.R
@@ -0,0 +1,16 @@
+## EXERCISE 11
+## BAYESIAN ANALYSIS
+## learning how to use brm versions of analyses I have already done this semester
+## 
+## Luke Watson
+## 11/12/24
+
+
+# load libraries ----------------------------------------------------------
+
+
+# load data ---------------------------------------------------------------
+
+
+
+
